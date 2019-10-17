2f63b6a19814549ad96d6a9460246e4b1aa33bf7
Mu <muserygood@gmail.com>
2019-10-17 13:17:10 +0800

    修改导航栏

--- src/config/env.js
+++ src/config/env.js
@@ -11,7 +11,7 @@ let routerMode = 'hash';
 let baseImgPath;
 
 if (process.env.NODE_ENV == 'development') {
-	baseUrl = 'http://192.168.1.13:9999';
+	baseUrl = 'http://localhost:9999';
     baseImgPath = '/img/';
 }else{
 	baseUrl = '//47.99.207.148/api';
